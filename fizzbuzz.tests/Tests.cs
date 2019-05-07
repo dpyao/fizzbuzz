95c296a73f02a446e13f4507df5282a84ffb5347
dpyao <anlihuer@outlook.com>
2019-05-07 20:08:32 +0800

    Print number from 1 to 100

--- fizzbuzz.tests/Tests.cs
+++ fizzbuzz.tests/Tests.cs
@@ -9,7 +9,12 @@ namespace fizzbuzz.tests
         public void Test1()
         {
             var program = new Program();
-            Assert.True(true);
+            var actual = program.getPrintedList();
+
+            Assert.Equal(100, actual.Count);
+            Assert.Equal(1, actual[0]);
+            Assert.Equal(50, actual[49]);
+            Assert.Equal(100, actual[99]);
         }
     }
 }
--- fizzbuzz/Program.cs
+++ fizzbuzz/Program.cs
@@ -1,9 +1,29 @@
-﻿namespace fizzbuzz
+﻿using System.Collections.Generic;
+
+namespace fizzbuzz
 {
     public class Program
     {
         public static void Main(string[] args)
         {
+            var program = new Program();
+            var printedList = program.getPrintedList();
+
+            foreach (var i in printedList)
+            {
+                System.Console.WriteLine(i);
+            }
+        }
+
+        public List<int> getPrintedList()
+        {
+            var printedList = new List<int>();
+
+            for (var i = 1; i <= 100; i++)
+            {
+                printedList.Add(i);
+            }
+            return printedList;
         }
     }
 }
